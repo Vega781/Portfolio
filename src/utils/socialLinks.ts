468ebc462eacd3c780d3f1d21441783d6196a6c1
Vega7 <anmax3@mail.ru>
2024-12-20 13:49:32 +0300

    added socialLinks file

---
+++ src/utils/socialLinks.ts
@@ -0,0 +1,8 @@
+export const socialLinks = {
+    github: 'https://github.com/Vega781',
+    telegram: 'https://t.me/Vega781',
+    hh: 'https://hh.ru/resume_converter/Анисенко%20Максим%20Андреевич.pdf?hash=ef39ebb2ff0d13a02c0039ed1f577968764970&type=pdf&hhtmSource=resume&hhtmFrom=resume_list',
+    hexlet: 'https://cv.hexlet.io/ru/resumes/13626',
+    email: 'mailto:anmax3@mail.ru',
+    fakecrime: 'https://fakecrime.bio/vega7',
+}
